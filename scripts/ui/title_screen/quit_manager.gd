f0890fb5af600e9c59e5d0c5210603ac359c6169
KOFLazycat <KOFLAzycat@163.com>
2025-05-22 23:49:36 +0800

    1. 修复类名冲突的BUG

---
+++ scripts/ui/title_screen/quit_manager.gd
@@ -0,0 +1,11 @@
+class_name QuitManager
+extends Node
+
+@export var button:Button
+
+func _ready()->void:
+	button.pressed.connect(pressed, CONNECT_ONE_SHOT)
+
+func pressed()->void:
+	Log.stop_session()
+	get_tree().quit()
